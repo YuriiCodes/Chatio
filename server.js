f30cc49668da74ef98dfe1420ae3a74915a3624d
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2022-11-03 23:15:04 +0200

    logging update

--- server.js
+++ server.js
@@ -20,13 +20,13 @@ let messages = []
 
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    console.log(new Date() +  ' :a user connected');
 
     socket.on('newMsg', (data) => {
         let message = {name: data.name, message: data.message}
 
         messages.push(message)
-        console.log("saved new chat")
+        console.log(new Date() +  ": saved new message from : " + data.name + " : " + data.message.substring(0, 10) + "...")
 
         io.emit('newMsg', {
             name: data.name,
@@ -37,12 +37,11 @@ io.on('connection', (socket) => {
 
 
 app.get('/', (req, res) => {
-    console.log("new connection on root");
-    console.log(messages.forEach(obj => console.log(obj.name, obj.message)))
+    console.log(new Date() + " :new connection on root");
     res.render('chatroom', {title: "Test ChatRoom", data: messages})
 })
 
 
 server.listen(PORT, (req, res) => {
-    console.log(`server running on ${PORT}`)
+    console.log(`${new Date()} :server running on ${PORT}`)
 })
